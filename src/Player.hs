4c0c50c2a0870cba26014d83c34063239634bd1b
csum112 <cataremix15@gmail.com>
2020-05-26 01:35:26 +0300

    Can deal cards now

--- src/Player.hs
+++ src/Player.hs
@@ -3,15 +3,35 @@ import Control.Concurrent
 import System.IO
 import Cards
 
-data PlayerWrapper = PW (MVar PlayerAction)
+data PlayerWrapper = PW (MVar PlayerAction) [Card]
 
-data PlayerAction = Discard [Card] | Claim
+data PlayerAction = 
+    Discard [Card] | 
+    Claim | 
+    BlindPick |
+    Pick Card |
+    AnnounceTurn | 
+    GameState [Card] Card
+
+data Game = GM [PlayerWrapper] [Card] Card
 
 
 cback :: Handle -> MVar PlayerAction-> IO ()
 cback hdl m = do
-    hPutStrLn hdl "Hello! Try to say something"
-    hPutStrLn hdl (renderCardList [(Card Spades Jack),(Card Hearts Queen),(Card Hearts Queen),(Card Hearts Queen),(Card Hearts Queen)])
-    hPutStrLn hdl ("Some blocking action")
-    req <- hGetLine hdl
+    hPutStrLn hdl "Welcome! We are waiting for the other players to join"
+    -- hPutStrLn hdl (renderCardList [(Card Spades Jack),(Card Hearts Queen),(Card Hearts Queen),(Card Hearts Queen),(Card Hearts Queen)])
+    (GameState hand last_card) <- takeMVar m
+    startPlaying hdl m hand last_card
     hClose hdl
+
+startPlaying :: Handle -> MVar PlayerAction -> [Card] -> Card ->  IO()
+startPlaying hdl m hand last_card = do
+    hPutStrLn hdl (renderCardList hand)
+    new_hand <- playRound hdl m hand
+    startPlaying hdl m new_hand last_card
+
+
+playRound :: Handle -> MVar PlayerAction -> [Card] -> IO [Card]
+playRound hdl m hand = do 
+    foo <- getLine
+    return hand
--- src/Server.hs
+++ src/Server.hs
@@ -5,6 +5,7 @@ import Network.Socket
 import Cards
 import Control.Concurrent
 import Player
+import Data.Tuple
 
 serverLoop :: String -> String -> Int -> IO ()
 serverLoop hostname port numberOfPlayers = do
@@ -13,16 +14,19 @@ serverLoop hostname port numberOfPlayers = do
     setSocketOption sock ReuseAddr 1   -- make socket immediately reusable - eases debugging.
     bind sock (addrAddress addr)  -- listen on TCP port 4242.
     listen sock 2                              -- set a max of 2 queued connections
-    gatherPlayers sock numberOfPlayers
-    gameLoop
+    players <- gatherPlayers sock numberOfPlayers
+    beginGame players
 
 
-gatherPlayers :: Socket -> Int -> IO ()
-gatherPlayers _ 0 = putStrLn "All players joined"
+gatherPlayers :: Socket -> Int -> IO ([PlayerWrapper])
+gatherPlayers _ 0 = do
+    putStrLn "All players joined"
+    return []
 gatherPlayers sock numberOfPlayers = do
     conn <- accept sock     -- accept a connection and handle it
     m <- runConn conn            -- run our server's logic
-    gatherPlayers sock (numberOfPlayers - 1)
+    otherPlayers <- gatherPlayers sock (numberOfPlayers - 1)
+    return ((PW m []):otherPlayers)
 
 runConn :: (Socket, SockAddr) -> IO (MVar PlayerAction)
 runConn (sock, _) = do
@@ -36,10 +40,62 @@ runConn (sock, _) = do
 getAnyAddr :: String -> String -> IO[AddrInfo]
 getAnyAddr hostname port = getAddrInfo (Just defaultHints) (Just hostname) (Just port)
 
-    
+
+-- =========TODO: Finish implementing
+
+beginGame :: [PlayerWrapper] -> IO ()
+beginGame players = do
+    putStrLn "Game has started. Now dealing cards"
+    shufDeck <- shuffle deck
+    game <- dealCardsGameWrapper players shufDeck numberOfCards
+    gameLoop game
+    where 
+        deck = deckBuilder
+        numberOfCards = 5
+
+dealCardsGameWrapper :: [PlayerWrapper] -> [Card] -> Int -> IO(Game)
+dealCardsGameWrapper players deck n = do 
+    return (GM newplayers newdeck last_card) where
+    (newplayers, newdeck) = dealCards players deck n
+    last_card = (Card Spades Ace) -- Fix this
+
+-- dealCards :: [PlayerWrapper] -> [Card] -> Int -> ([PlayerWrapper], [Card])
+-- dealCards players _ deck 0 = (players, deck)
+-- dealCards [] players deck n = (players, deck)
+-- dealCards (hd:tl) playersbak deck n = ((player:tl), newNewDeck) where
+--     ((_, newNewDeck), player) = ((dealCards tl playersbak newdeck n), newplayer) where
+--             (newplayer, newdeck) = drawCard hd deck
+
+dealCards :: [PlayerWrapper] -> [Card] -> Int -> ([PlayerWrapper], [Card])
+dealCards players deck 0 = (players, deck)
+dealCards players deck n = dealCards newplayers newdeck (n-1) where
+    (newplayers, newdeck) = dealCardsRound players deck
+
+dealCardsRound :: [PlayerWrapper] -> [Card] -> ([PlayerWrapper], [Card])
+dealCardsRound [] deck = ([], deck)
+dealCardsRound (hd: tl) deck = ((player:tl), newNewDeck) where
+    ((_, newNewDeck), player) = ((dealCardsRound tl newdeck), newplayer) where
+            (newplayer, newdeck) = drawCard hd deck
+
+drawCard :: PlayerWrapper -> [Card] -> (PlayerWrapper, [Card])
+drawCard (PW mvar hand) [] = ((PW mvar hand), [])
+drawCard (PW mvar hand) (hd:tl) = ((PW mvar newhand), tl) where
+    newhand = hd:hand
 
 
-gameLoop :: IO ()
-gameLoop = do
-    putStrLn "Not implemented yet"
+gameLoop :: Game -> IO()
+gameLoop (GM players deck last_card) = do 
+    putStrLn "Sending players the new game state"
+    updatePlayers players last_card
+    foo <- getLine
+    gameLoop (GM players deck last_card)
 
+
+updatePlayers :: [PlayerWrapper] -> Card -> IO()
+updatePlayers [] _ = return ()
+updatePlayers (hd:tl) last_card = do
+    updatePlayer hd last_card
+    updatePlayers tl last_card
+
+updatePlayer :: PlayerWrapper -> Card -> IO()
+updatePlayer (PW m hand) last_card = putMVar m (GameState hand last_card)
